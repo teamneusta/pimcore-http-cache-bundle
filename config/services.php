1cb093b913145f68eda6a775e816d01ca32d3413
Jacob Dreesen <j.dreesen@neusta.de>
2025-03-11 18:27:26 +0100

    Configure PurgeChecker according to the config
    Elements that are disabled in the config should also be disabled in the PurgeChecker.

--- config/services.php
+++ config/services.php
@@ -5,6 +5,7 @@ use Neusta\Pimcore\HttpCacheBundle\Cache\CacheInvalidationListener;
 use Neusta\Pimcore\HttpCacheBundle\Cache\CacheInvalidator;
 use Neusta\Pimcore\HttpCacheBundle\Cache\CacheInvalidatorInterface;
 use Neusta\Pimcore\HttpCacheBundle\Cache\CacheTagCollector;
+use Neusta\Pimcore\HttpCacheBundle\Cache\PurgeChecker;
 use Neusta\Pimcore\HttpCacheBundle\Cache\PurgeCheckerInterface;
 use Neusta\Pimcore\HttpCacheBundle\Cache\PurgeCheckerInterface;
 use Neusta\Pimcore\HttpCacheBundle\CacheActivator;
@@ -14,6 +15,7 @@ use Pimcore\Event\DataObjectEvents;
 use Pimcore\Event\DocumentEvents;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\Messenger\Event\WorkerMessageHandledEvent;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\abstract_arg;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $configurator) {
@@ -29,7 +31,8 @@ return static function (ContainerConfigurator $configurator) {
     $services->set(CacheTagCollector::class)
         ->arg('$responseTagger', service('fos_http_cache.http.symfony_response_tagger'));
 
-    $services->set(PurgeCheckerInterface::class, PurgeChecker::class);
+    $services->set(PurgeCheckerInterface::class, PurgeChecker::class)
+        ->arg('$types', abstract_arg('Set in the extension'));
 
     $services->set(CacheInvalidationListener::class)
         ->arg('$cacheManager', service(CacheManager::class))
--- src/DependencyInjection/NeustaPimcoreHttpCacheExtension.php
+++ src/DependencyInjection/NeustaPimcoreHttpCacheExtension.php
@@ -2,6 +2,8 @@
 
 namespace Neusta\Pimcore\HttpCacheBundle\DependencyInjection;
 
+use Neusta\Pimcore\HttpCacheBundle\Cache\PurgeCheckerInterface;
+use Neusta\Pimcore\HttpCacheBundle\Element\ElementType;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
@@ -27,5 +29,11 @@ final class NeustaPimcoreHttpCacheExtension extends ConfigurableExtension
         }
 
         $loader->load('services.php');
+
+        $container->getDefinition(PurgeCheckerInterface::class)->setArgument('$types', [
+            ElementType::Asset->value => $mergedConfig['asset'],
+            ElementType::Object->value => $mergedConfig['object'],
+            ElementType::Document->value => $mergedConfig['document'],
+        ]);
     }
 }
