c0f4c64b4ad2cb8d582ebd3ef402c21040a5c424
Jacob Dreesen <j.dreesen@neusta.de>
2025-03-11 18:03:23 +0100

    Prefix services with bundle name

--- config/services/fos_http_cache.php
+++ config/services/fos_http_cache.php
@@ -9,9 +9,9 @@ return static function (ContainerConfigurator $configurator) {
     $services = $configurator->services();
 
     $services->set('fos_http_cache.http.symfony_response_tagger', SymfonyResponseTagger::class)
-        ->arg('$headerFormatter', service('cache_invalidation.http_cache.comma_separated_tag_header_formatter'));
+        ->arg('$headerFormatter', service('neusta_pimcore_http_cache.tag_header_formatter'));
 
-    $services->set('cache_invalidation.http_cache.comma_separated_tag_header_formatter', CommaSeparatedTagHeaderFormatter::class)
+    $services->set('neusta_pimcore_http_cache.tag_header_formatter', CommaSeparatedTagHeaderFormatter::class)
         ->arg('$headerName', 'xkey')
         ->arg('$glue', ' ');
 };
