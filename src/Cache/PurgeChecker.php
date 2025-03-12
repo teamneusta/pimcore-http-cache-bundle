6c41792f2e9c90bc78151bef8004d2b2548d5c3a
Jacob Dreesen <j.dreesen@neusta.de>
2025-03-12 10:14:45 +0100

    Configure `PurgeChecker` according to the config (#2)
    * Register PurgeCheckerInterface instead of PurgeChecker as service
    
    * Configure PurgeChecker according to the config
    
    Elements that are disabled in the config should also be disabled in the PurgeChecker.

--- src/Cache/PurgeChecker.php
+++ src/Cache/PurgeChecker.php
@@ -4,30 +4,12 @@ namespace Neusta\Pimcore\HttpCacheBundle\Cache;
 
 final class PurgeChecker implements PurgeCheckerInterface
 {
-    public const TYPE_ASSET = 'asset';
-    public const TYPE_OBJECT = 'object';
-    public const TYPE_DOCUMENTS = 'document';
-
-    /**
-     * @var array<string, bool>
-     */
-    private const DEFAULT_TYPES = [
-        self::TYPE_ASSET => true,
-        self::TYPE_OBJECT => true,
-        self::TYPE_DOCUMENTS => true,
-    ];
-
-    /**
-     * @var array<string, bool>
-     */
-    private array $types;
-
     /**
      * @param array<string, bool> $types
      */
-    public function __construct(array $types = self::DEFAULT_TYPES)
-    {
-        $this->types = $types;
+    public function __construct(
+        private array $types,
+    ) {
     }
 
     public function isEnabled(string $type): bool
