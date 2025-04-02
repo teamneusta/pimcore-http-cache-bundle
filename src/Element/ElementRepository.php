aff0477b0757ba7a6061d95e0061384530ab00a5
Jacob Dreesen <j.dreesen@neusta.de>
2025-04-02 14:34:48 +0200

    Refactor to use ElementRepository for element retrieval
    Introduced `ElementRepository` to centralize element retrieval logic. Updated `ElementCacheTagChecker` to use the repository for fetching assets, documents, and objects, improving modularity and maintainability. Registered `ElementRepository` as an inline service in the configuration.

---
+++ src/Element/ElementRepository.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace Neusta\Pimcore\HttpCacheBundle\Element;
+
+use Pimcore\Model\Asset;
+use Pimcore\Model\DataObject;
+use Pimcore\Model\Document;
+
+/**
+ * @internal
+ *
+ * @final
+ */
+class ElementRepository
+{
+    public function findAsset(int $id): ?Asset
+    {
+        return Asset::getById($id);
+    }
+
+    public function findDocument(int $id): ?Document
+    {
+        return Document::getById($id);
+    }
+
+    public function findObject(int $id): ?DataObject
+    {
+        return DataObject::getById($id);
+    }
+}
