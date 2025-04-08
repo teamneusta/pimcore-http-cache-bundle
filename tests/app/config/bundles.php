cad4e03fcd2c1687e07ff5a2e7118caf44970f6a
Jacob Dreesen <j.dreesen@neusta.de>
2025-04-08 18:33:28 +0200

    Remove unnecessary config

--- tests/app/config/bundles.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-use Neusta\Pimcore\HttpCacheBundle\NeustaPimcoreHttpCacheBundle;
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    NeustaPimcoreHttpCacheBundle::class => ['all' => true],
-];
