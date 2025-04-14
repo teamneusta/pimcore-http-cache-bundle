face77ece32efa6ed6d4957455984541c2c9c05f
jadams <j.adams@neusta.de>
2025-04-14 12:38:05 +0200

    Fix listener not tagged with the right events

--- tests/app/config/services.php
+++ tests/app/config/services.php
@@ -5,8 +5,6 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 use App\Service\CancelInvalidationListener;
 use App\Service\ClearRuntimeListener;
 use App\Service\InvalidateAdditionalTagListener;
-use Neusta\Pimcore\HttpCacheBundle\Cache\CacheInvalidatorInterface;
-use Neusta\Pimcore\HttpCacheBundle\Element\InvalidateElementListener;
 use Pimcore\Event\AssetEvents;
 use Pimcore\Event\DataObjectEvents;
 use Pimcore\Event\DocumentEvents;
@@ -33,15 +31,4 @@ return function (ContainerConfigurator $container): void {
 
     $services->set(CancelInvalidationListener::class)
         ->public();
-
-    // TODO: Remove!
-    $services->set(InvalidateElementListener::class)
-        ->arg('$cacheInvalidator', service(CacheInvalidatorInterface::class))
-        ->arg('$dispatcher', service('event_dispatcher'))
-        ->tag('kernel.event_listener', ['event' => AssetEvents::POST_UPDATE, 'method' => 'onUpdated'])
-        ->tag('kernel.event_listener', ['event' => AssetEvents::POST_DELETE, 'method' => 'onDeleted'])
-        ->tag('kernel.event_listener', ['event' => DataObjectEvents::POST_UPDATE, 'method' => 'onUpdated'])
-        ->tag('kernel.event_listener', ['event' => DataObjectEvents::POST_DELETE, 'method' => 'onDeleted'])
-        ->tag('kernel.event_listener', ['event' => DocumentEvents::POST_UPDATE, 'method' => 'onUpdated'])
-        ->tag('kernel.event_listener', ['event' => DocumentEvents::POST_DELETE, 'method' => 'onDeleted']);
 };
