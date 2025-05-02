c8c8903feb1e85693bf9c156c77a24517876d934
jadams <j.adams@neusta.de>
2025-05-02 13:16:28 +0200

    Move to sub folder

--- tests/Unit/Cache/CacheTagChecker/ElementCacheTagCheckerTest.php
+++ tests/Unit/Cache/CacheTagChecker/ElementCacheTagCheckerTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Neusta\Pimcore\HttpCacheBundle\Tests\Unit\Cache;
+namespace Neusta\Pimcore\HttpCacheBundle\Tests\Unit\Cache\CacheTagChecker;
 
 use Neusta\Pimcore\HttpCacheBundle\Cache\CacheTag;
 use Neusta\Pimcore\HttpCacheBundle\Cache\CacheTagChecker;
