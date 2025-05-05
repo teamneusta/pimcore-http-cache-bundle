c678eed1016147a52b38567bbee9b695e4b51fd2
jadams <j.adams@neusta.de>
2025-05-05 13:06:56 +0200

    Testcases for disabling object types

--- tests/Integration/Invalidation/InvalidateObjectTest.php
+++ tests/Integration/Invalidation/InvalidateObjectTest.php
@@ -9,6 +9,7 @@ use Neusta\Pimcore\TestingFramework\Database\ResetDatabase;
 use Neusta\Pimcore\TestingFramework\Test\Attribute\ConfigureExtension;
 use Neusta\Pimcore\TestingFramework\Test\Attribute\ConfigureRoute;
 use Neusta\Pimcore\TestingFramework\Test\ConfigurableKernelTestCase;
+use Pimcore\Model\DataObject;
 use Pimcore\Model\DataObject\TestDataObject;
 use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
@@ -26,13 +27,18 @@ final class InvalidateObjectTest extends ConfigurableKernelTestCase
 
     private TestDataObject $object;
 
+    private TestDataObject $variant;
+
+    private DataObject\Folder $folder;
+
     protected function setUp(): void
     {
         $this->cacheManager = $this->prophesize(CacheManager::class);
         $this->cacheManager->invalidateTags(Argument::any())->willReturn($this->cacheManager->reveal());
         self::getContainer()->set('fos_http_cache.cache_manager', $this->cacheManager->reveal());
-
-        $this->object = self::arrange(fn () => TestObjectFactory::simple()->save());
+        $this->object = self::arrange(fn () => TestObjectFactory::simpleObject()->save());
+        $this->folder = self::arrange(fn () => TestObjectFactory::simpleFolder()->save());
+        $this->variant = self::arrange(fn () => TestObjectFactory::simpleVariant()->save());
     }
 
     /**
@@ -63,5 +69,145 @@ final class InvalidateObjectTest extends ConfigurableKernelTestCase
         $this->object->delete();
 
         $this->cacheManager->invalidateTags(['o42'])->shouldHaveBeenCalledTimes(1);
+    }
+
+    /**
+     * @test
+     */
+    #[ConfigureExtension('neusta_pimcore_http_cache', [
+        'elements' => [
+            'objects' => true,
+        ],
+    ])]
+    public function response_is_not_invalidated_object_is_of_type_folder_on_update(): void
+    {
+        $this->folder->setKey('updated_test_object_folder')->save();
+
+        $this->cacheManager->invalidateTags(['o23'])->shouldNotHaveBeenCalled();
+    }
+
+    /**
+     * @test
+     */
+    #[ConfigureExtension('neusta_pimcore_http_cache', [
+        'elements' => [
+            'objects' => true,
+        ],
+    ])]
+    public function response_is_not_invalidated_object_is_of_type_folder_on_delete(): void
+    {
+        $this->folder->delete();
+
+        $this->cacheManager->invalidateTags(['o23'])->shouldNotHaveBeenCalled();
+    }
+
+    /**
+     * @test
+     */
+    #[ConfigureExtension('neusta_pimcore_http_cache', [
+        'elements' => [
+            'objects' => [
+                'enabled' => true,
+                'types' => [
+                    'variant' => true,
+                ],
+            ],
+        ],
+    ])]
+    public function response_is_invalidated_when_specified_type_is_enabled_on_update(): void
+    {
+        $this->variant->setContent('Updated test content')->save();
+
+        $this->cacheManager->invalidateTags(['o17'])->shouldHaveBeenCalledOnce();
+    }
+
+    /**
+     * @test
+     */
+    #[ConfigureExtension('neusta_pimcore_http_cache', [
+        'elements' => [
+            'objects' => [
+                'enabled' => true,
+                'types' => [
+                    'variant' => false,
+                ],
+            ],
+        ],
+    ])]
+    public function response_is_not_invalidated_when_specified_type_is_disabled_on_delete(): void
+    {
+        $this->variant->delete();
+
+        $this->cacheManager->invalidateTags(['o42'])->shouldNotHaveBeenCalled();
+    }
+
+    /**
+     * @test
+     */
+    #[ConfigureExtension('neusta_pimcore_http_cache', [
+        'elements' => [
+            'objects' => [
+                'enabled' => true,
+                'classes' => [
+                    'TestDataObject' => false,
+                ],
+            ],
+        ],
+    ])]
+    public function response_is_not_invalidated_when_custom_data_object_class_is_disabled_on_update(): void
+    {
+        $this->object->setContent('Updated test content')->save();
+
+        $this->cacheManager->invalidateTags(['o42'])->shouldNotHaveBeenCalled();
+    }
+
+    /**
+     * @test
+     */
+    #[ConfigureExtension('neusta_pimcore_http_cache', [
+        'elements' => [
+            'objects' => [
+                'enabled' => true,
+                'classes' => [
+                    'TestDataObject' => false,
+                ],
+            ],
+        ],
+    ])]
+    public function response_is_not_invalidated_when_custom_data_object_class_is_disabled_on_delete(): void
+    {
+        $this->object->delete();
+
+        $this->cacheManager->invalidateTags(['o42'])->shouldNotHaveBeenCalled();
+    }
+
+    /**
+     * @test
+     */
+    #[ConfigureExtension('neusta_pimcore_http_cache', [
+        'elements' => [
+            'objects' => false,
+        ],
+    ])]
+    public function response_is_not_invalidated_when_objects_is_disabled_on_update(): void
+    {
+        $this->object->setContent('Updated test content')->save();
+
+        $this->cacheManager->invalidateTags(['o42'])->shouldNotHaveBeenCalled();
+    }
+
+    /**
+     * @test
+     */
+    #[ConfigureExtension('neusta_pimcore_http_cache', [
+        'elements' => [
+            'objects' => false,
+        ],
+    ])]
+    public function response_is_not_invalidated_when_objects_is_disabled_on_delete(): void
+    {
+        $this->object->delete();
+
+        $this->cacheManager->invalidateTags(['o42'])->shouldNotHaveBeenCalled();
     }
 }
