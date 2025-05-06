8d7b411d7880829dd91dc12001bd736eb5f4c201
jadams <j.adams@neusta.de>
2025-05-06 07:24:32 +0200

    Add testcases for custom cache type

--- tests/Integration/Invalidation/InvalidateAdditionalTagTest.php
+++ tests/Integration/Invalidation/InvalidateAdditionalTagTest.php
@@ -4,6 +4,7 @@ namespace Neusta\Pimcore\HttpCacheBundle\Tests\Integration\Invalidation;
 
 use FOS\HttpCacheBundle\CacheManager;
 use Neusta\Pimcore\HttpCacheBundle\Cache\CacheTag;
+use Neusta\Pimcore\HttpCacheBundle\Cache\CacheType\CustomCacheType;
 use Neusta\Pimcore\HttpCacheBundle\Element\ElementInvalidationEvent;
 use Neusta\Pimcore\HttpCacheBundle\Tests\Integration\Helpers\ArrangeCacheTest;
 use Neusta\Pimcore\HttpCacheBundle\Tests\Integration\Helpers\TestAssetFactory;
@@ -11,7 +12,6 @@ use Neusta\Pimcore\HttpCacheBundle\Tests\Integration\Helpers\TestDocumentFactory
 use Neusta\Pimcore\HttpCacheBundle\Tests\Integration\Helpers\TestObjectFactory;
 use Neusta\Pimcore\TestingFramework\Database\ResetDatabase;
 use Neusta\Pimcore\TestingFramework\Test\Attribute\ConfigureExtension;
-use Neusta\Pimcore\TestingFramework\Test\Attribute\ConfigureRoute;
 use Neusta\Pimcore\TestingFramework\Test\ConfigurableKernelTestCase;
 use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
@@ -34,7 +34,7 @@ final class InvalidateAdditionalTagTest extends ConfigurableKernelTestCase
 
         self::getContainer()->get('event_dispatcher')->addListener(
             ElementInvalidationEvent::class,
-            fn ($event) => $event->cacheTags->add(CacheTag::fromString('additional_tag')),
+            fn ($event) => $event->cacheTags->add(CacheTag::fromString('bar', new CustomCacheType('foo'))),
         );
     }
 
@@ -45,6 +45,9 @@ final class InvalidateAdditionalTagTest extends ConfigurableKernelTestCase
         'elements' => [
             'objects' => true,
         ],
+        'cache_types' => [
+            'foo' => true,
+        ],
     ])]
     public function invalidate_additional_tag_on_object_update(): void
     {
@@ -52,41 +55,7 @@ final class InvalidateAdditionalTagTest extends ConfigurableKernelTestCase
 
         $object->setContent('Updated test content')->save();
 
-        $this->cacheManager->invalidateTags(['additional_tag'])->shouldHaveBeenCalledTimes(1);
-    }
-
-    /**
-     * @test
-     */
-    #[ConfigureExtension('neusta_pimcore_http_cache', [
-        'elements' => [
-            'documents' => true,
-        ],
-    ])]
-    public function invalidate_additional_tag_on_document_update(): void
-    {
-        $document = self::arrange(fn () => TestDocumentFactory::simplePage()->save());
-
-        $document->setKey('updated_test_document_page')->save();
-
-        $this->cacheManager->invalidateTags(['additional_tag'])->shouldHaveBeenCalledTimes(1);
-    }
-
-    /**
-     * @test
-     */
-    #[ConfigureExtension('neusta_pimcore_http_cache', [
-        'elements' => [
-            'assets' => true,
-        ],
-    ])]
-    public function invalidate_additional_tag_on_asset_update(): void
-    {
-        $asset = self::arrange(fn () => TestAssetFactory::simpleAsset()->save());
-
-        $asset->setData('Updated test content')->save();
-
-        $this->cacheManager->invalidateTags(['additional_tag'])->shouldHaveBeenCalledTimes(1);
+        $this->cacheManager->invalidateTags(['foo-bar'])->shouldHaveBeenCalledTimes(1);
     }
 
     /**
@@ -96,31 +65,17 @@ final class InvalidateAdditionalTagTest extends ConfigurableKernelTestCase
         'elements' => [
             'objects' => true,
         ],
+        'cache_types' => [
+            'foo' => false,
+        ],
     ])]
-    public function invalidate_additional_tag_on_object_deletion(): void
+    public function does_not_invalidate_additional_tag_on_object_update_when_cache_type_is_disabled(): void
     {
         $object = self::arrange(fn () => TestObjectFactory::simpleObject()->save());
 
-        $object->delete();
+        $object->setKey('updated_test_object')->save();
 
-        $this->cacheManager->invalidateTags(['additional_tag'])->shouldHaveBeenCalledTimes(1);
-    }
-
-    /**
-     * @test
-     */
-    #[ConfigureExtension('neusta_pimcore_http_cache', [
-        'elements' => [
-            'assets' => true,
-        ],
-    ])]
-    public function invalidate_additional_tag_on_asset_deletion(): void
-    {
-        $asset = self::arrange(fn () => TestAssetFactory::simpleAsset()->save());
-
-        $asset->delete();
-
-        $this->cacheManager->invalidateTags(['additional_tag'])->shouldHaveBeenCalledTimes(1);
+        $this->cacheManager->invalidateTags(['foo-bar'])->shouldNotHaveBeenCalled();
     }
 
     /**
@@ -130,6 +85,169 @@ final class InvalidateAdditionalTagTest extends ConfigurableKernelTestCase
         'elements' => [
             'documents' => true,
         ],
+        'cache_types' => [
+            'foo' => true,
+        ],
+    ])]
+    public function invalidate_additional_tag_on_document_update(): void
+    {
+        $document = self::arrange(fn () => TestDocumentFactory::simplePage()->save());
+
+        $document->setKey('updated_test_document_page')->save();
+
+        $this->cacheManager->invalidateTags(['foo-bar'])->shouldHaveBeenCalledTimes(1);
+    }
+
+    /**
+     * @test
+     */
+    #[ConfigureExtension('neusta_pimcore_http_cache', [
+        'elements' => [
+            'documents' => true,
+        ],
+        'cache_types' => [
+            'foo' => false,
+        ],
+    ])]
+    public function does_not_invalidate_additional_tag_on_document_update_when_cache_type_is_disabled(): void
+    {
+        $document = self::arrange(fn () => TestDocumentFactory::simplePage()->save());
+
+        $document->setKey('updated_test_document_page')->save();
+
+        $this->cacheManager->invalidateTags(['foo-bar'])->shouldNotHaveBeenCalled();
+    }
+
+    /**
+     * @test
+     */
+    #[ConfigureExtension('neusta_pimcore_http_cache', [
+        'elements' => [
+            'assets' => true,
+        ],
+        'cache_types' => [
+            'foo' => true,
+        ],
+    ])]
+    public function invalidate_additional_tag_on_asset_update(): void
+    {
+        $asset = self::arrange(fn () => TestAssetFactory::simpleAsset()->save());
+
+        $asset->setData('Updated test content')->save();
+
+        $this->cacheManager->invalidateTags(['foo-bar'])->shouldHaveBeenCalledTimes(1);
+    }
+
+    /**
+     * @test
+     */
+    #[ConfigureExtension('neusta_pimcore_http_cache', [
+        'elements' => [
+            'assets' => true,
+        ],
+        'cache_types' => [
+            'foo' => false,
+        ],
+    ])]
+    public function does_not_invalidate_additional_tag_on_asset_update_when_cache_type_is_disabled(): void
+    {
+        $asset = self::arrange(fn () => TestAssetFactory::simpleAsset()->save());
+
+        $asset->setData('Updated test content')->save();
+
+        $this->cacheManager->invalidateTags(['foo-bar'])->shouldNotHaveBeenCalled();
+    }
+
+    /**
+     * @test
+     */
+    #[ConfigureExtension('neusta_pimcore_http_cache', [
+        'elements' => [
+            'objects' => true,
+        ],
+        'cache_types' => [
+            'foo' => true,
+        ],
+    ])]
+    public function invalidate_additional_tag_on_object_deletion(): void
+    {
+        $object = self::arrange(fn () => TestObjectFactory::simpleObject()->save());
+
+        $object->delete();
+
+        $this->cacheManager->invalidateTags(['foo-bar'])->shouldHaveBeenCalledTimes(1);
+    }
+
+    /**
+     * @test
+     */
+    #[ConfigureExtension('neusta_pimcore_http_cache', [
+        'elements' => [
+            'objects' => true,
+        ],
+        'cache_types' => [
+            'foo' => false,
+        ],
+    ])]
+    public function does_not_invalidate_additional_tag_on_object_deletion_when_cache_type_is_disabled(): void
+    {
+        $object = self::arrange(fn () => TestObjectFactory::simpleObject()->save());
+
+        $object->delete();
+
+        $this->cacheManager->invalidateTags(['foo-bar'])->shouldNotHaveBeenCalled();
+    }
+
+    /**
+     * @test
+     */
+    #[ConfigureExtension('neusta_pimcore_http_cache', [
+        'elements' => [
+            'assets' => true,
+        ],
+        'cache_types' => [
+            'foo' => true,
+        ],
+    ])]
+    public function invalidate_additional_tag_on_asset_deletion(): void
+    {
+        $asset = self::arrange(fn () => TestAssetFactory::simpleAsset()->save());
+
+        $asset->delete();
+
+        $this->cacheManager->invalidateTags(['foo-bar'])->shouldHaveBeenCalledTimes(1);
+    }
+
+    /**
+     * @test
+     */
+    #[ConfigureExtension('neusta_pimcore_http_cache', [
+        'elements' => [
+            'assets' => true,
+        ],
+        'cache_types' => [
+            'foo' => false,
+        ],
+    ])]
+    public function does_not_invalidate_additional_tag_on_asset_deletion_when_cache_type_is_disabled(): void
+    {
+        $asset = self::arrange(fn () => TestAssetFactory::simpleAsset()->save());
+
+        $asset->delete();
+
+        $this->cacheManager->invalidateTags(['foo-bar'])->shouldNotHaveBeenCalled();
+    }
+
+    /**
+     * @test
+     */
+    #[ConfigureExtension('neusta_pimcore_http_cache', [
+        'elements' => [
+            'documents' => true,
+        ],
+        'cache_types' => [
+            'foo' => true,
+        ],
     ])]
     public function invalidate_additional_tag_on_document_deletion(): void
     {
@@ -137,6 +255,26 @@ final class InvalidateAdditionalTagTest extends ConfigurableKernelTestCase
 
         $document->delete();
 
-        $this->cacheManager->invalidateTags(['additional_tag'])->shouldHaveBeenCalledTimes(1);
+        $this->cacheManager->invalidateTags(['foo-bar'])->shouldHaveBeenCalledTimes(1);
+    }
+
+    /**
+     * @test
+     */
+    #[ConfigureExtension('neusta_pimcore_http_cache', [
+        'elements' => [
+            'documents' => true,
+        ],
+        'cache_types' => [
+            'foo' => false,
+        ],
+    ])]
+    public function does_not_invalidate_additional_tag_on_document_deletion_when_cache_type_was_disabled(): void
+    {
+        $document = self::arrange(fn () => TestDocumentFactory::simplePage()->save());
+
+        $document->delete();
+
+        $this->cacheManager->invalidateTags(['foo-bar'])->shouldNotHaveBeenCalled();
     }
 }
