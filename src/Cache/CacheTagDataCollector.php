cd05e06c5930cbcc774ad2ba2143ae46d698bce5
jadams <j.adams@neusta.de>
2025-05-19 16:31:19 +0200

    Display cache tags

---
+++ src/Cache/CacheTagDataCollector.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace Neusta\Pimcore\HttpCacheBundle\Cache;
+
+use Neusta\Pimcore\HttpCacheBundle\Cache\ResponseTagger\CollectTagsResponseTagger;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+use Symfony\Component\HttpKernel\DataCollector\LateDataCollectorInterface;
+
+final class CacheTagDataCollector extends DataCollector implements LateDataCollectorInterface
+{
+    public function __construct(
+        private readonly CollectTagsResponseTagger $cacheTagCollector,
+    ) {
+    }
+
+    public function collect(Request $request, Response $response, ?\Throwable $exception = null): void
+    {
+        // We'll collect data in lateCollect() since tags are added after the response is created
+    }
+
+    public function lateCollect(): void
+    {
+        foreach ($this->cacheTagCollector->collectedTags->tags as $tag) {
+            $this->data['tags'][] = [
+                'tag' => $tag->toString(), 'type' => $tag->type->toString(),
+            ];
+        }
+    }
+
+    /**
+     * @return array<array{tag: string, type: string}>
+     */
+    public function getTags(): array
+    {
+        return $this->data['tags'] ?? [];
+    }
+
+    public function getName(): string
+    {
+        return 'cache_tags';
+    }
+
+    public function reset(): void
+    {
+        $this->data['tags'] = [];
+    }
+}
---
+++ src/Cache/ResponseTagger/CollectTagsResponseTagger.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Neusta\Pimcore\HttpCacheBundle\Cache\ResponseTagger;
+
+use Neusta\Pimcore\HttpCacheBundle\Cache\CacheTags;
+use Neusta\Pimcore\HttpCacheBundle\Cache\ResponseTagger;
+
+final class CollectTagsResponseTagger implements ResponseTagger
+{
+    public CacheTags $collectedTags;
+
+    public function __construct(
+        private readonly ResponseTagger $inner,
+    ) {
+        $this->collectedTags = new CacheTags();
+    }
+
+    public function tag(CacheTags $tags): void
+    {
+        $this->collectedTags->addTags($tags);
+
+        $this->inner->tag($tags);
+    }
+}
